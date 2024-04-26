288621f6cc8b6e9a241c0be9532d0996e282d0e3
cipher <dilsurbet@hmail.com>
2024-04-26 22:45:05 +0500

    4oy5darsuyishi

---
+++ app.js
@@ -0,0 +1,12 @@
+// 1 vazifa
+
+let A = parseInt(prompt("A raqamini kiriting: "));
+let B = parseInt(prompt("B raqamini kiriting: "));
+let C = 0;
+
+function calc(A, B) {
+  return (C = A + B);
+}
+
+calc();
+console.log(C);
