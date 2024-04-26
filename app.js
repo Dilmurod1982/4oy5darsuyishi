511ee337b8090faee2be34860e079cbee77d673b
cipher <dilsurbet@hmail.com>
2024-04-26 23:12:36 +0500

    6 vazifa

--- app.js
+++ app.js
@@ -1,12 +1,67 @@
 // 1 vazifa
 
-let A = parseInt(prompt("A raqamini kiriting: "));
-let B = parseInt(prompt("B raqamini kiriting: "));
-let C = 0;
+// let A = parseInt(prompt("A raqamini kiriting: "));
+// let B = parseInt(prompt("B raqamini kiriting: "));
+// let C = 0;
 
-function calc(A, B) {
-  return (C = A + B);
+// function calc(A, B) {
+//   return (C = A + B);
+// }
+
+// calc(A, B);
+// alert(`A va B raqamlar yeg'indisi: ${C}`);
+
+// 2 vazifa
+
+// let min = parseInt(prompt("Minut kiriting: "));
+
+// function mintosec(min) {
+//   return (sec = min * 60);
+// }
+// mintosec(min);
+// alert(`Kiritilgan minutda jami ${sec} sekund mavjud`);
+
+// 3 vazifa
+
+// let son = parseInt(prompt("Ixtiyoriy butun son kiriting: "));
+
+// function number(son) {
+//   return (raqam = son + 1);
+// }
+// number(son);
+// alert(`Kiritilgan sonongizdan keyingi son ${raqam} `);
+
+// 4 vazifa
+
+// let asos = parseInt(prompt("Uchburchakni asosini kiriting: "));
+// let baland = parseInt(prompt("Uchburchakni balandligini kiriting: "));
+
+// function uch(asos, baland) {
+//   return (yuz = (asos * baland) / 2);
+// }
+// uch(asos, baland);
+// alert(`Uchburchak maydoni: ${yuz} `);
+
+// 5 vazifa
+
+// let age = parseInt(prompt("Yoshni kiriting: "));
+
+// function ageToDay(age) {
+//   return (day = age * 365);
+// }
+// ageToDay(age);
+// alert(`Siz ${day} kun yahsyapsiz!`);
+
+// 6 vazifa
+
+let cub = parseInt(prompt("Raqamni kiriting: "));
+
+function toCub(cub) {
+  return (result = cub ** 3);
 }
+toCub(cub);
+alert(`Kiritgan raqamingizni 3 darajasi ${result} `);
 
-calc();
-console.log(C);
+// 7 vazifa
+
+
