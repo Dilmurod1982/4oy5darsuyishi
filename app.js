567ee1d478455d8c02da267cadc029de95afbdc7
cipher <dilsurbet@hmail.com>
2024-04-27 19:18:13 +0500

    1

--- app.js
+++ app.js
@@ -54,14 +54,123 @@
 
 // 6 vazifa
 
-let cub = parseInt(prompt("Raqamni kiriting: "));
+// let cub = parseInt(prompt("Raqamni kiriting: "));
 
-function toCub(cub) {
-  return (result = cub ** 3);
-}
-toCub(cub);
-alert(`Kiritgan raqamingizni 3 darajasi ${result} `);
+// function toCub(cub) {
+//   return (result = cub ** 3);
+// }
+// toCub(cub);
+// alert(`Kiritgan raqamingizni 3 darajasi ${result} `);
 
 // 7 vazifa
 
+// const myArray = [1, 2, 3, 4, 5];
+// const firstElement = getFirstElement(myArray);
+// console.log(firstElement);
 
+// function getFirstElement(arr) {
+//   return arr[0];
+// }
+
+// 8 vazifa
+
+// let A = parseInt(prompt("A raqamini kiriting: "));
+// let B = parseInt(prompt("B raqamini kiriting: "));
+// let C = 0;
+
+// function calc(A, B) {
+//   return (C = A * B);
+// }
+
+// calc(A, B);
+// alert(`A va B raqamlar kopaytmasi: ${C}`);
+
+// 9 vazifa
+
+// let soat = parseInt(prompt("Soat kiriting: "));
+
+// function hourtosec(soat) {
+//   return (sec = soat * 60 * 60);
+// }
+// hourtosec(soat);
+// alert(`Kiritilgan soatda jami ${sec} sekund mavjud`);
+
+// 10 vazifa
+
+// let A = parseInt(prompt("Uchburchakni bir tomonini kiriting: "));
+// let B = parseInt(prompt("Uchburchakni ikkinchi tomonini kiriting: "));
+// let C;
+
+// function calc(A, B) {
+//   C = A + B - 1;
+//   return C;
+// }
+
+// calc(A, B);
+// alert(`Uchburchakni 3 chi tomoni ${C}`);
+
+// 11 vazifa
+
+// let A = parseInt(prompt("Birinchi sonni kiriting: "));
+// let B = parseInt(prompt("Ikkinchi sonni kiriting: "));
+// let C;
+
+// function calc(A, B) {
+//   C = A % B;
+//   return C;
+// }
+
+// calc(A, B);
+// alert(`Qoldiq: ${C}`);
+
+// 12 vazifa
+
+// let A = parseInt(prompt("Tortburchkni balandligini kiriting: "));
+// let B = parseInt(prompt("Tortburchkni enini kiriting: "));
+// let C;
+
+// function calc(A, B) {
+//   C = (A + B) * 2;
+//   return C;
+// }
+
+// calc(A, B);
+// alert(`4 burchakni perimetri: ${C}`);
+
+// 13 vazifa
+
+// let A = prompt(`String ma'lumot kiriting:`);
+// function str(A) {
+//   return (B = A + " " + "a");
+// }
+
+// str(A);
+// alert(B);
+
+// 14 vazifa
+
+// let A = parseInt(prompt("Son kiriting: "));
+// let B;
+
+// function calc(A) {
+//   return (B = A ** 2);
+// }
+
+// calc(A);
+// alert(`Kiritgan soningizni kvadrati ${B} teng`);
+
+// 15 vazifa
+
+let A = parseInt(prompt("Son kiriting: "));
+let B;
+
+function agar(son) {
+  if (A <= 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+agar(A);
+alert(agar);
